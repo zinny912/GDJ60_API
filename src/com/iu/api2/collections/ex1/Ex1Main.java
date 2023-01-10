79bdecb97a354bccb1c580c6483c1e9e4e3986d0
zinny912 <xbigbang5@gmail.com>
2023-01-10 09:24:23 +0900

    gg

---
+++ src/com/iu/api2/collections/ex1/Ex1Main.java
@@ -0,0 +1,15 @@
+package com.iu.api2.collections.ex1;
+
+public class Ex1Main {
+
+	public static void main(String[] args) {
+		//학생은 이름 번호 국어 영어 수학 총점 평균으로 이루어짐
+		//1. 학생정보초기화
+		//2. 학생정보전체조회
+		//3. 학생정보검색조회(이름검색)
+		//4. 학생정보추가
+		//5. 학생정보삭제(이름)
+		//6. 프로그램종료
+	}
+
+}
---
+++ src/com/iu/api2/collections/ex1/StudentDAO.java
@@ -0,0 +1,17 @@
+package com.iu.api2.collections.ex1;
+
+public class StudentDAO {
+	
+	private StringBuffer sb;
+	
+	public StudentDAO() {
+		this.sb=new StringBuffer();
+		
+		sb.append("iu-1-90-60-70-");
+		sb.append("winter-2-86-84-75-");
+		sb.append("suji, 3, 89, 74, 87 ");
+		sb.append("choa, 4, 71, 25, 99 ");
+	
+	}
+
+}
